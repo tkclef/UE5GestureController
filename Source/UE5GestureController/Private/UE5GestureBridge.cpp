481083ba10fb18cea2f6454697c97fdb13974a03
Tkclef <tkclefofficial@gmail.com>
2026-07-12 04:15:47 +0000

    UE5GestureBridge  | created actor class

---
+++ Source/UE5GestureController/Private/UE5GestureBridge.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UE5GestureBridge.h"
+
+// Sets default values
+AUE5GestureBridge::AUE5GestureBridge()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AUE5GestureBridge::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AUE5GestureBridge::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
