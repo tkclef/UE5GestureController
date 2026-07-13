d5ea84b18c3430022aaa2c7f42a5ae79f9da9094
Tkclef <tkclefofficial@gmail.com>
2026-07-13 04:28:52 +0000

    OSCReceiver C++ created

---
+++ Source/UE5GestureController/Private/OSCReceiver.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OSCReceiver.h"
+
+// Sets default values
+AOSCReceiver::AOSCReceiver()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AOSCReceiver::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AOSCReceiver::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
